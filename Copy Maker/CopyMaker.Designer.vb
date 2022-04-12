d011a1e7f4c7a6457d9e3455a07f0b3f703f4491
Syed Bilal Abbas <abbasb000@gmail.com>
2022-04-12 19:32:54 +0500

    Destination Folder added

--- Copy Maker/CopyMaker.Designer.vb
+++ Copy Maker/CopyMaker.Designer.vb
@@ -22,34 +22,39 @@ Partial Class CopyMaker
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.btnBrowseSourceFile = New System.Windows.Forms.Button()
         Me.dbLoadFile = New System.Windows.Forms.OpenFileDialog()
-        Me.lblLoadFile = New System.Windows.Forms.Label()
+        Me.lblPathSourceFile = New System.Windows.Forms.Label()
         Me.btnStart = New System.Windows.Forms.Button()
         Me.txtNames = New System.Windows.Forms.TextBox()
+        Me.lblSourceFile = New System.Windows.Forms.Label()
+        Me.lblDestinationFolder = New System.Windows.Forms.Label()
+        Me.lblPathDestinationFolder = New System.Windows.Forms.Label()
+        Me.btnBrowseDestinationFolder = New System.Windows.Forms.Button()
+        Me.dbFolderBrowser = New System.Windows.Forms.FolderBrowserDialog()
         Me.SuspendLayout()
         '
-        'btnBrowse
+        'btnBrowseSourceFile
         '
-        Me.btnBrowse.Location = New System.Drawing.Point(44, 98)
-        Me.btnBrowse.Name = "btnBrowse"
-        Me.btnBrowse.Size = New System.Drawing.Size(94, 29)
-        Me.btnBrowse.TabIndex = 0
-        Me.btnBrowse.Text = "Browse"
-        Me.btnBrowse.UseVisualStyleBackColor = True
+        Me.btnBrowseSourceFile.Location = New System.Drawing.Point(163, 28)
+        Me.btnBrowseSourceFile.Name = "btnBrowseSourceFile"
+        Me.btnBrowseSourceFile.Size = New System.Drawing.Size(94, 29)
+        Me.btnBrowseSourceFile.TabIndex = 0
+        Me.btnBrowseSourceFile.Text = "Browse"
+        Me.btnBrowseSourceFile.UseVisualStyleBackColor = True
         '
         'dbLoadFile
         '
         Me.dbLoadFile.FileName = "OpenFileDialog1"
         '
-        'lblLoadFile
+        'lblPathSourceFile
         '
-        Me.lblLoadFile.AutoSize = True
-        Me.lblLoadFile.Location = New System.Drawing.Point(167, 102)
-        Me.lblLoadFile.Name = "lblLoadFile"
-        Me.lblLoadFile.Size = New System.Drawing.Size(45, 20)
-        Me.lblLoadFile.TabIndex = 1
-        Me.lblLoadFile.Text = "None"
+        Me.lblPathSourceFile.AutoSize = True
+        Me.lblPathSourceFile.Location = New System.Drawing.Point(281, 32)
+        Me.lblPathSourceFile.Name = "lblPathSourceFile"
+        Me.lblPathSourceFile.Size = New System.Drawing.Size(45, 20)
+        Me.lblPathSourceFile.TabIndex = 1
+        Me.lblPathSourceFile.Text = "None"
         '
         'btnStart
         '
@@ -62,22 +67,62 @@ Partial Class CopyMaker
         '
         'txtNames
         '
-        Me.txtNames.Location = New System.Drawing.Point(44, 149)
+        Me.txtNames.Location = New System.Drawing.Point(23, 147)
         Me.txtNames.Multiline = True
         Me.txtNames.Name = "txtNames"
         Me.txtNames.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
         Me.txtNames.Size = New System.Drawing.Size(282, 229)
         Me.txtNames.TabIndex = 3
         '
+        'lblSourceFile
+        '
+        Me.lblSourceFile.AutoSize = True
+        Me.lblSourceFile.Location = New System.Drawing.Point(23, 32)
+        Me.lblSourceFile.Name = "lblSourceFile"
+        Me.lblSourceFile.Size = New System.Drawing.Size(84, 20)
+        Me.lblSourceFile.TabIndex = 4
+        Me.lblSourceFile.Text = "Source File:"
+        '
+        'lblDestinationFolder
+        '
+        Me.lblDestinationFolder.AutoSize = True
+        Me.lblDestinationFolder.Location = New System.Drawing.Point(23, 67)
+        Me.lblDestinationFolder.Name = "lblDestinationFolder"
+        Me.lblDestinationFolder.Size = New System.Drawing.Size(134, 20)
+        Me.lblDestinationFolder.TabIndex = 7
+        Me.lblDestinationFolder.Text = "Destination Folder:"
+        '
+        'lblPathDestinationFolder
+        '
+        Me.lblPathDestinationFolder.AutoSize = True
+        Me.lblPathDestinationFolder.Location = New System.Drawing.Point(281, 67)
+        Me.lblPathDestinationFolder.Name = "lblPathDestinationFolder"
+        Me.lblPathDestinationFolder.Size = New System.Drawing.Size(45, 20)
+        Me.lblPathDestinationFolder.TabIndex = 6
+        Me.lblPathDestinationFolder.Text = "None"
+        '
+        'btnBrowseDestinationFolder
+        '
+        Me.btnBrowseDestinationFolder.Location = New System.Drawing.Point(163, 63)
+        Me.btnBrowseDestinationFolder.Name = "btnBrowseDestinationFolder"
+        Me.btnBrowseDestinationFolder.Size = New System.Drawing.Size(94, 29)
+        Me.btnBrowseDestinationFolder.TabIndex = 5
+        Me.btnBrowseDestinationFolder.Text = "Browse"
+        Me.btnBrowseDestinationFolder.UseVisualStyleBackColor = True
+        '
         'CopyMaker
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblDestinationFolder)
+        Me.Controls.Add(Me.lblPathDestinationFolder)
+        Me.Controls.Add(Me.btnBrowseDestinationFolder)
+        Me.Controls.Add(Me.lblSourceFile)
         Me.Controls.Add(Me.txtNames)
         Me.Controls.Add(Me.btnStart)
-        Me.Controls.Add(Me.lblLoadFile)
-        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.lblPathSourceFile)
+        Me.Controls.Add(Me.btnBrowseSourceFile)
         Me.Name = "CopyMaker"
         Me.Text = "Copy Maker"
         Me.ResumeLayout(False)
@@ -85,9 +130,14 @@ Partial Class CopyMaker
 
     End Sub
 
-    Friend WithEvents btnBrowse As Button
+    Friend WithEvents btnBrowseSourceFile As Button
     Friend WithEvents dbLoadFile As OpenFileDialog
-    Friend WithEvents lblLoadFile As Label
+    Friend WithEvents lblPathSourceFile As Label
     Friend WithEvents btnStart As Button
     Friend WithEvents txtNames As TextBox
+    Friend WithEvents lblSourceFile As Label
+    Friend WithEvents lblDestinationFolder As Label
+    Friend WithEvents lblPathDestinationFolder As Label
+    Friend WithEvents btnBrowseDestinationFolder As Button
+    Friend WithEvents dbFolderBrowser As FolderBrowserDialog
 End Class
--- Copy Maker/CopyMaker.vb
+++ Copy Maker/CopyMaker.vb
@@ -2,49 +2,58 @@
 Imports System.IO
 
 Public Class CopyMaker
-    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowseSourceFile.Click
         If dbLoadFile.ShowDialog <> Windows.Forms.DialogResult.Cancel Then
-            lblLoadFile.Text = dbLoadFile.SafeFileName
+            lblPathSourceFile.Text = dbLoadFile.SafeFileName
+            'lblPathSourceFile.Text = System.IO.Path.GetFileNameWithoutExtension(dbLoadFile.FileName)
+
+
         End If
     End Sub
 
     Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
 
-        If lblLoadFile.Text = "None" Then
+        If lblPathSourceFile.Text = "None" Or lblPathDestinationFolder.Text = "None" Then
             MsgBox("Please select path first", vbCritical, "Error")
+        Else
+            Dim sourceFile As String = dbLoadFile.FileName
+
+
+            'Creating folder with file name
+            If Not System.IO.Directory.Exists(lblPathDestinationFolder.Text + "/" + System.IO.Path.GetFileNameWithoutExtension(dbLoadFile.FileName)) Then
+                System.IO.Directory.CreateDirectory(lblPathDestinationFolder.Text + "/" + System.IO.Path.GetFileNameWithoutExtension(dbLoadFile.FileName))
+            End If
+
+
+
+            For Each strLine As String In txtNames.Text.Split(vbCrLf)
+                'TextBox1.Text &= strLine & Environment.NewLine
+
+                Dim fileName As String = Trim(strLine)
+                If fileName IsNot "" Then
+
+                    'TextBox1.AppendText(i & Environment.NewLine)
+
+
+                    Dim destinationFile As String = lblPathDestinationFolder.Text + "/" + System.IO.Path.GetFileNameWithoutExtension(dbLoadFile.FileName) + "/" + fileName + Path.GetExtension(dbLoadFile.FileName)
+                    clone(sourceFile, destinationFile)
+                End If
+            Next
+
+            For Each i In txtNames.Text
+                Dim fileName As String = i
+                If fileName IsNot " " Then
+
+                    'TextBox1.AppendText(i & Environment.NewLine)
+
+
+                    'Dim destinationFile As String = "D:\" + fileName + Path.GetExtension(dbLoadFile.FileName)
+                    'clone(sourceFile, destinationFile)
+                End If
+
+            Next
         End If
 
-
-        Dim sourceFile As String = dbLoadFile.FileName
-
-        For Each strLine As String In txtNames.Text.Split(vbNewLine)
-            'TextBox1.Text &= strLine & Environment.NewLine
-
-            Dim fileName As String = Trim(strLine)
-            If fileName IsNot "" Then
-
-                'TextBox1.AppendText(i & Environment.NewLine)
-
-
-                Dim destinationFile As String = "D:\" + fileName + Path.GetExtension(dbLoadFile.FileName)
-                clone(sourceFile, destinationFile)
-            End If
-
-
-        Next
-
-        For Each i In txtNames.Text
-            Dim fileName As String = i
-            If fileName IsNot " " Then
-
-                'TextBox1.AppendText(i & Environment.NewLine)
-
-
-                'Dim destinationFile As String = "D:\" + fileName + Path.GetExtension(dbLoadFile.FileName)
-                'clone(sourceFile, destinationFile)
-            End If
-
-        Next
 
     End Sub
 
@@ -59,4 +68,15 @@ Public Class CopyMaker
             Console.WriteLine(iox.Message)
         End Try
     End Function
+
+    Private Sub btnBrowseDestinationFolder_Click(sender As Object, e As EventArgs) Handles btnBrowseDestinationFolder.Click
+        If (dbFolderBrowser.ShowDialog() = DialogResult.OK) Then
+            lblPathDestinationFolder.Text = dbFolderBrowser.SelectedPath
+        End If
+
+
+
+
+    End Sub
+
 End Class

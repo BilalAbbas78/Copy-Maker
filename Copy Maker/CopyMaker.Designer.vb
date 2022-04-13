f8b726d2f08acda8ccee8f031d3f3fae4a61f93e
Syed Bilal Abbas <abbasb000@gmail.com>
2022-04-13 15:13:50 +0500

    Final UI Changes

--- Copy Maker/CopyMaker.Designer.vb
+++ Copy Maker/CopyMaker.Designer.vb
@@ -24,125 +24,134 @@ Partial Class CopyMaker
     Private Sub InitializeComponent()
         Me.btnBrowseSourceFile = New System.Windows.Forms.Button()
         Me.dbLoadFile = New System.Windows.Forms.OpenFileDialog()
-        Me.lblPathSourceFile = New System.Windows.Forms.Label()
-        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnExecute = New System.Windows.Forms.Button()
         Me.txtNames = New System.Windows.Forms.TextBox()
-        Me.lblSourceFile = New System.Windows.Forms.Label()
-        Me.lblOutputFolder = New System.Windows.Forms.Label()
-        Me.lblPathOutputFolder = New System.Windows.Forms.Label()
         Me.btnBrowseOutputFolder = New System.Windows.Forms.Button()
         Me.dbFolderBrowser = New System.Windows.Forms.FolderBrowserDialog()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPathSourceFile = New System.Windows.Forms.TextBox()
+        Me.txtPathOutputFolder = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'btnBrowseSourceFile
         '
-        Me.btnBrowseSourceFile.BackColor = System.Drawing.Color.White
-        Me.btnBrowseSourceFile.Location = New System.Drawing.Point(122, 28)
+        Me.btnBrowseSourceFile.BackColor = System.Drawing.Color.RoyalBlue
+        Me.btnBrowseSourceFile.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnBrowseSourceFile.ForeColor = System.Drawing.Color.White
+        Me.btnBrowseSourceFile.Location = New System.Drawing.Point(415, 90)
         Me.btnBrowseSourceFile.Name = "btnBrowseSourceFile"
-        Me.btnBrowseSourceFile.Size = New System.Drawing.Size(94, 29)
+        Me.btnBrowseSourceFile.Size = New System.Drawing.Size(154, 27)
         Me.btnBrowseSourceFile.TabIndex = 0
-        Me.btnBrowseSourceFile.Text = "Browse"
+        Me.btnBrowseSourceFile.Text = "COPY FILE"
         Me.btnBrowseSourceFile.UseVisualStyleBackColor = False
         '
-        'lblPathSourceFile
+        'btnExecute
         '
-        Me.lblPathSourceFile.AutoSize = True
-        Me.lblPathSourceFile.ForeColor = System.Drawing.Color.White
-        Me.lblPathSourceFile.Location = New System.Drawing.Point(12, 58)
-        Me.lblPathSourceFile.Name = "lblPathSourceFile"
-        Me.lblPathSourceFile.Size = New System.Drawing.Size(45, 20)
-        Me.lblPathSourceFile.TabIndex = 1
-        Me.lblPathSourceFile.Text = "None"
-        '
-        'btnStart
-        '
-        Me.btnStart.Location = New System.Drawing.Point(182, 493)
-        Me.btnStart.Name = "btnStart"
-        Me.btnStart.Size = New System.Drawing.Size(94, 29)
-        Me.btnStart.TabIndex = 3
-        Me.btnStart.Text = "Start"
-        Me.btnStart.UseVisualStyleBackColor = True
+        Me.btnExecute.BackColor = System.Drawing.Color.RoyalBlue
+        Me.btnExecute.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnExecute.ForeColor = System.Drawing.Color.White
+        Me.btnExecute.Location = New System.Drawing.Point(599, 111)
+        Me.btnExecute.Name = "btnExecute"
+        Me.btnExecute.Size = New System.Drawing.Size(103, 33)
+        Me.btnExecute.TabIndex = 3
+        Me.btnExecute.Text = "EXECUTE"
+        Me.btnExecute.UseVisualStyleBackColor = False
         '
         'txtNames
         '
         Me.txtNames.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.txtNames.Location = New System.Drawing.Point(12, 179)
+        Me.txtNames.ForeColor = System.Drawing.Color.Black
+        Me.txtNames.Location = New System.Drawing.Point(12, 207)
         Me.txtNames.Multiline = True
         Me.txtNames.Name = "txtNames"
         Me.txtNames.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.txtNames.Size = New System.Drawing.Size(438, 293)
+        Me.txtNames.Size = New System.Drawing.Size(709, 293)
         Me.txtNames.TabIndex = 2
-        '
-        'lblSourceFile
-        '
-        Me.lblSourceFile.AutoSize = True
-        Me.lblSourceFile.ForeColor = System.Drawing.Color.White
-        Me.lblSourceFile.Location = New System.Drawing.Point(12, 32)
-        Me.lblSourceFile.Name = "lblSourceFile"
-        Me.lblSourceFile.Size = New System.Drawing.Size(84, 20)
-        Me.lblSourceFile.TabIndex = 4
-        Me.lblSourceFile.Text = "Source File:"
-        '
-        'lblOutputFolder
-        '
-        Me.lblOutputFolder.AutoSize = True
-        Me.lblOutputFolder.ForeColor = System.Drawing.Color.White
-        Me.lblOutputFolder.Location = New System.Drawing.Point(12, 94)
-        Me.lblOutputFolder.Name = "lblOutputFolder"
-        Me.lblOutputFolder.Size = New System.Drawing.Size(104, 20)
-        Me.lblOutputFolder.TabIndex = 7
-        Me.lblOutputFolder.Text = "Output Folder:"
-        '
-        'lblPathOutputFolder
-        '
-        Me.lblPathOutputFolder.AutoSize = True
-        Me.lblPathOutputFolder.ForeColor = System.Drawing.Color.White
-        Me.lblPathOutputFolder.Location = New System.Drawing.Point(12, 120)
-        Me.lblPathOutputFolder.Name = "lblPathOutputFolder"
-        Me.lblPathOutputFolder.Size = New System.Drawing.Size(45, 20)
-        Me.lblPathOutputFolder.TabIndex = 6
-        Me.lblPathOutputFolder.Text = "None"
         '
         'btnBrowseOutputFolder
         '
-        Me.btnBrowseOutputFolder.BackColor = System.Drawing.Color.White
-        Me.btnBrowseOutputFolder.Location = New System.Drawing.Point(122, 90)
+        Me.btnBrowseOutputFolder.BackColor = System.Drawing.Color.RoyalBlue
+        Me.btnBrowseOutputFolder.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnBrowseOutputFolder.ForeColor = System.Drawing.Color.White
+        Me.btnBrowseOutputFolder.Location = New System.Drawing.Point(416, 131)
         Me.btnBrowseOutputFolder.Name = "btnBrowseOutputFolder"
-        Me.btnBrowseOutputFolder.Size = New System.Drawing.Size(94, 29)
+        Me.btnBrowseOutputFolder.Size = New System.Drawing.Size(154, 27)
         Me.btnBrowseOutputFolder.TabIndex = 1
-        Me.btnBrowseOutputFolder.Text = "Browse"
+        Me.btnBrowseOutputFolder.Text = "OUTPUT LOCATION"
         Me.btnBrowseOutputFolder.UseVisualStyleBackColor = False
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(12, 156)
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(12, 184)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(123, 20)
+        Me.Label1.Size = New System.Drawing.Size(209, 20)
         Me.Label1.TabIndex = 8
-        Me.Label1.Text = "Input File Names:"
+        Me.Label1.Text = "Enter output file names here ..."
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.RoyalBlue
+        Me.PictureBox1.Location = New System.Drawing.Point(-15, -2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(760, 57)
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.RoyalBlue
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(293, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(150, 35)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "CopyMaker"
+        '
+        'txtPathSourceFile
+        '
+        Me.txtPathSourceFile.BackColor = System.Drawing.Color.White
+        Me.txtPathSourceFile.Location = New System.Drawing.Point(12, 90)
+        Me.txtPathSourceFile.Name = "txtPathSourceFile"
+        Me.txtPathSourceFile.ReadOnly = True
+        Me.txtPathSourceFile.Size = New System.Drawing.Size(405, 27)
+        Me.txtPathSourceFile.TabIndex = 11
+        '
+        'txtPathOutputFolder
+        '
+        Me.txtPathOutputFolder.BackColor = System.Drawing.Color.White
+        Me.txtPathOutputFolder.Location = New System.Drawing.Point(12, 132)
+        Me.txtPathOutputFolder.Name = "txtPathOutputFolder"
+        Me.txtPathOutputFolder.ReadOnly = True
+        Me.txtPathOutputFolder.Size = New System.Drawing.Size(405, 27)
+        Me.txtPathOutputFolder.TabIndex = 12
         '
         'CopyMaker
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.Navy
-        Me.ClientSize = New System.Drawing.Size(476, 534)
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(733, 518)
+        Me.Controls.Add(Me.txtPathOutputFolder)
+        Me.Controls.Add(Me.txtPathSourceFile)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.lblOutputFolder)
-        Me.Controls.Add(Me.lblPathOutputFolder)
         Me.Controls.Add(Me.btnBrowseOutputFolder)
-        Me.Controls.Add(Me.lblSourceFile)
         Me.Controls.Add(Me.txtNames)
-        Me.Controls.Add(Me.btnStart)
-        Me.Controls.Add(Me.lblPathSourceFile)
+        Me.Controls.Add(Me.btnExecute)
         Me.Controls.Add(Me.btnBrowseSourceFile)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.Name = "CopyMaker"
-        Me.Text = "Copy Maker"
+        Me.Text = "CopyMaker"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -150,13 +159,13 @@ Partial Class CopyMaker
 
     Friend WithEvents btnBrowseSourceFile As Button
     Friend WithEvents dbLoadFile As OpenFileDialog
-    Friend WithEvents lblPathSourceFile As Label
-    Friend WithEvents btnStart As Button
+    Friend WithEvents btnExecute As Button
     Friend WithEvents txtNames As TextBox
-    Friend WithEvents lblSourceFile As Label
-    Friend WithEvents lblOutputFolder As Label
-    Friend WithEvents lblPathOutputFolder As Label
     Friend WithEvents btnBrowseOutputFolder As Button
     Friend WithEvents dbFolderBrowser As FolderBrowserDialog
     Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPathSourceFile As TextBox
+    Friend WithEvents txtPathOutputFolder As TextBox
 End Class
--- Copy Maker/CopyMaker.vb
+++ Copy Maker/CopyMaker.vb
@@ -3,32 +3,41 @@
 Public Class CopyMaker
     Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowseSourceFile.Click
         If dbLoadFile.ShowDialog <> Windows.Forms.DialogResult.Cancel Then
-            lblPathSourceFile.Text = dbLoadFile.SafeFileName
+            txtPathSourceFile.Text = dbLoadFile.SafeFileName
         End If
     End Sub
 
-    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnExecute.Click
 
-        If lblPathSourceFile.Text = "None" Or lblPathOutputFolder.Text = "None" Then
+        If txtPathSourceFile.Text = "" Or txtPathOutputFolder.Text = "" Then
             MsgBox("Please select path first", vbCritical, "Error")
         Else
             Dim sourceFile As String = dbLoadFile.FileName
 
             'Creating folder with file name
-            If Not System.IO.Directory.Exists(lblPathOutputFolder.Text + "/" + System.IO.Path.GetFileNameWithoutExtension(dbLoadFile.FileName)) Then
-                System.IO.Directory.CreateDirectory(lblPathOutputFolder.Text + "/" + System.IO.Path.GetFileNameWithoutExtension(dbLoadFile.FileName))
+            If Not System.IO.Directory.Exists(txtPathOutputFolder.Text + "/" + System.IO.Path.GetFileNameWithoutExtension(dbLoadFile.FileName)) Then
+                System.IO.Directory.CreateDirectory(txtPathOutputFolder.Text + "/" + System.IO.Path.GetFileNameWithoutExtension(dbLoadFile.FileName))
             End If
 
             For Each strLine As String In txtNames.Text.Split(vbCrLf)
 
                 Dim fileName As String = Trim(strLine)
                 If fileName IsNot "" Then
-                    Dim destinationFile As String = lblPathOutputFolder.Text + "/" + System.IO.Path.GetFileNameWithoutExtension(dbLoadFile.FileName) + "/" + fileName + Path.GetExtension(dbLoadFile.FileName)
+                    Dim destinationFile As String = txtPathOutputFolder.Text + "/" + System.IO.Path.GetFileNameWithoutExtension(dbLoadFile.FileName) + "/" + fileName + Path.GetExtension(dbLoadFile.FileName)
                     clone(sourceFile, destinationFile)
                 End If
             Next
+
+            MsgBox("Task completed successfully", vbInformation, "Completed")
+
         End If
 
+    End Sub
+
+    Private Sub btnBrowseDestinationFolder_Click(sender As Object, e As EventArgs) Handles btnBrowseOutputFolder.Click
+        If (dbFolderBrowser.ShowDialog() = DialogResult.OK) Then
+            txtPathOutputFolder.Text = dbFolderBrowser.SelectedPath
+        End If
     End Sub
 
     Private Function clone(sourceFile As String, destinationFile As String)
@@ -40,11 +49,5 @@ Public Class CopyMaker
         End Try
         Return Nothing
     End Function
-
-    Private Sub btnBrowseDestinationFolder_Click(sender As Object, e As EventArgs) Handles btnBrowseOutputFolder.Click
-        If (dbFolderBrowser.ShowDialog() = DialogResult.OK) Then
-            lblPathOutputFolder.Text = dbFolderBrowser.SelectedPath
-        End If
-    End Sub
 
 End Class
